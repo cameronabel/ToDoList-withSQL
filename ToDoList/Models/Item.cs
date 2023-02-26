96328863ec1faa38c56be888163fc436c1c9b068
Cameron Abel <cameronabel@gmail.com>
2023-02-26 11:19:13 -0800

    DB save test pass
    Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com>
    Co-authored-by: Brian Yoder <brianyoder@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,7 +6,7 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
-    public int Id { get; }
+    public int Id { get; set; }
 
     public Item(string description)
     {
@@ -66,7 +66,28 @@ namespace ToDoList.Models
     }
     public void Save()
     {
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+      conn.Open();
 
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+
+      // Begin new code
+
+      cmd.CommandText = "INSERT INTO items (description) VALUES (@ItemDescription);";
+      MySqlParameter param = new MySqlParameter();
+      param.ParameterName = "@ItemDescription";
+      param.Value = this.Description;
+      cmd.Parameters.Add(param);
+      cmd.ExecuteNonQuery();
+      Id = (int)cmd.LastInsertedId;
+
+      // End new code
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
 
     public override bool Equals(System.Object otherItem)
@@ -78,8 +99,9 @@ namespace ToDoList.Models
       else
       {
         Item newItem = (Item)otherItem;
+        bool idEquality = (this.Id == newItem.Id);
         bool descriptionEquality = (this.Description == newItem.Description);
-        return descriptionEquality;
+        return (descriptionEquality && idEquality);
       }
     }
     public override int GetHashCode()
